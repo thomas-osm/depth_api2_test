c411a6a2581508ef5b4734762d3df3bb9d9b2137
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2012-10-08 19:02:48 +0000

    B: ensure preference presence

--- net.sf.seesea.osm/src/net/sf/seesea/osm/preferences/OSMPreferenceInitializer.java
+++ net.sf.seesea.osm/src/net/sf/seesea/osm/preferences/OSMPreferenceInitializer.java
@@ -1,6 +1,6 @@
 /**
  * 
- Copyright (c) 2010-2012, Jens Kübler All rights reserved.
+ Copyright (c) 2010-2012, Jens Kï¿½bler All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -31,9 +31,11 @@ import net.sf.seesea.osm.OpenSeaMapActivator;
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
@@ -45,14 +47,30 @@ public class OSMPreferenceInitializer extends AbstractPreferenceInitializer {
 		String tileSource = "http://tile.openstreetmap.org/"; //$NON-NLS-1$
 		preferenceStore.setDefault(IOSMPreferences.TILE_SOURCE, tileSource);
 
-		
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = workspace.getRoot();
 		try {
 			IProject project = root.getProject("NMEALogging"); //$NON-NLS-1$
+			if (!project.exists()) {
+				project.create(new NullProgressMonitor());
+			}
+			if (!project.isOpen()) {
+				project.open(null);
+			}
+
+			IFolder logFolder = project.getFolder("logs"); //$NON-NLS-1$
+			if (!logFolder.exists()) {
+				logFolder.create(IResource.NONE, true, null);
+			}
 			IFolder cacheFolder = project.getFolder("tilecache"); //$NON-NLS-1$
-			String dir = cacheFolder.getLocationURI().toURL().getFile();
-			preferenceStore.setDefault("cacheDirectory", dir); //$NON-NLS-1$
+			if (!cacheFolder.exists()) {
+				cacheFolder.create(IResource.NONE, true, null);
+			}
+
+			if(cacheFolder.getLocationURI() != null) {
+				String dir = cacheFolder.getLocationURI().toURL().getFile();
+				preferenceStore.setDefault("cacheDirectory", dir); //$NON-NLS-1$
+			}
 		} catch (Exception e) {
 			Logger.getLogger(getClass()).error("Failed to retrieve tile cache directory", e); //$NON-NLS-1$
 		}
