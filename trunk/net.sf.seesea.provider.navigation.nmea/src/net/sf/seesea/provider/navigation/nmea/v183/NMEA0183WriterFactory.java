946aeab1165a5f78dd0605dd6ba6fb9976f4fb2c
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2013-02-10 00:25:56 +0000

    F: Rework filter processing

--- trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/v183/NMEA0183WriterFactory.java
+++ trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/v183/NMEA0183WriterFactory.java
@@ -11,11 +11,15 @@ import net.sf.seesea.data.io.WriterException;
 
 public class NMEA0183WriterFactory implements IWriterFactory {
 
+	private long fileCounter = 1;
+
 	@Override
 	public IDataWriter createWriter(Map<String,String> parameters) throws WriterException {
 		FileOutputStream outputStream;
 		try {
-			String filename = parameters.get("filename");
+			String fileprefix = parameters.get("fileprefix"); //$NON-NLS-1$
+			String filesuffix = parameters.get("filesuffix"); //$NON-NLS-1$
+			String filename = fileprefix + fileCounter++ + filesuffix;
 			outputStream = new FileOutputStream(filename);
 			return new NMEA0183Writer(outputStream);
 		} catch (FileNotFoundException e) {
