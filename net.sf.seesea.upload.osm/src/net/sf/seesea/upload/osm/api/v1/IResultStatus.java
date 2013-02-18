56caea71d25df44b13e892978a920406878499a5
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2013-02-18 16:55:19 +0000

    F: Add upload api

--- net.sf.seesea.upload.osm/src/net/sf/seesea/upload/osm/api/v1/IResultStatus.java
+++
@@ -1,8 +0,0 @@
-package net.sf.seesea.upload.osm.api.v1;
-
-import org.eclipse.core.runtime.IStatus;
-
-public interface IResultStatus<T> extends IStatus {
-
-	T getResult();
-}
--- net.sf.seesea.upload.osm/src/net/sf/seesea/upload/osm/api/v1/Track.java
+++ net.sf.seesea.upload.osm/src/net/sf/seesea/upload/osm/api/v1/Track.java
@@ -1,9 +1,36 @@
+/**
+ * 
+ Copyright (c) 2010-2013, Jens Kübler All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. Neither the name of the <organization> nor the names
+ * of its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
 package net.sf.seesea.upload.osm.api.v1;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.MessageFormat;
 
+import net.sf.seesea.lib.ResultStatus;
 import net.sf.seesea.upload.osm.OSMUploadActivator;
 
 import org.apache.commons.httpclient.HttpClient;
@@ -63,7 +90,7 @@ public class Track {
 		}
 	}
 
-	public ResultStatus<Integer> upload(String sessionId, Integer trackId,
+	public ResultStatus<Long> upload(String sessionId, Integer trackId,
 			File track) {
 		HttpClient client = new HttpClient();
 		PostMethod method = null;
@@ -77,36 +104,36 @@ public class Track {
 					method.getParams()));
 			int statusCode = client.executeMethod(method);
 			if (statusCode == 200) {
-				int length = Integer.parseInt(method.getResponseBodyAsString());
-				return new ResultStatus<Integer>(length, IStatus.OK,
+				long length = Long.parseLong(method.getResponseBodyAsString());
+				return new ResultStatus<Long>(length, IStatus.OK,
 						OSMUploadActivator.PLUGIN_ID, "OK");
 			} else if (statusCode == 202) {
-				return new ResultStatus<Integer>(IStatus.ERROR,
+				return new ResultStatus<Long>(IStatus.ERROR,
 						OSMUploadActivator.PLUGIN_ID, "Invalid track id");
 			} else if (statusCode == 203) {
-				return new ResultStatus<Integer>(IStatus.ERROR,
+				return new ResultStatus<Long>(IStatus.ERROR,
 						OSMUploadActivator.PLUGIN_ID, "Upload already done");
 			} else if (statusCode == 803) {
-				return new ResultStatus<Integer>(IStatus.ERROR,
+				return new ResultStatus<Long>(IStatus.ERROR,
 						OSMUploadActivator.PLUGIN_ID,
 						"Header X-Track Id not set");
 			} else if (statusCode == 804) {
-				return new ResultStatus<Integer>(IStatus.ERROR,
+				return new ResultStatus<Long>(IStatus.ERROR,
 						OSMUploadActivator.PLUGIN_ID,
 						"Missing filed name track");
 			} else if (statusCode == 999) {
-				return new ResultStatus<Integer>(IStatus.ERROR,
+				return new ResultStatus<Long>(IStatus.ERROR,
 						OSMUploadActivator.PLUGIN_ID, "Server side error");
 			} else {
-				return new ResultStatus<Integer>(IStatus.ERROR,
+				return new ResultStatus<Long>(IStatus.ERROR,
 						OSMUploadActivator.PLUGIN_ID, "Unknown Error");
 			}
 		} catch (HttpException e) {
-			return new ResultStatus<Integer>(IStatus.ERROR,
+			return new ResultStatus<Long>(IStatus.ERROR,
 					OSMUploadActivator.PLUGIN_ID, "Failed to contact server.",
 					e);
 		} catch (IOException e) {
-			return new ResultStatus<Integer>(IStatus.ERROR,
+			return new ResultStatus<Long>(IStatus.ERROR,
 					OSMUploadActivator.PLUGIN_ID, "Failed to contact server.",
 					e);
 		} finally {
