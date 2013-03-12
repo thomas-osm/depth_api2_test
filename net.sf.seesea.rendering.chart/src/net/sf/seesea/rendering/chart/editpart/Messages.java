857941356033fdacfb39da55c528984072182d34
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2013-03-12 21:55:26 +0000

    F: AIS i18n and rate of turn display, mmsi

--- net.sf.seesea.rendering.chart/src/net/sf/seesea/rendering/chart/editpart/Messages.java
+++ net.sf.seesea.rendering.chart/src/net/sf/seesea/rendering/chart/editpart/Messages.java
@@ -1,6 +1,6 @@
 /**
  * 
- Copyright (c) 2010-2012, Jens Kübler All rights reserved.
+ Copyright (c) 2010-2012, Jens Kï¿½bler All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -30,7 +30,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "net.sf.seesea.ui.editpart.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "net.sf.seesea.rendering.chart.editpart.Messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
