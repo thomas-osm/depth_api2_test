b1b7d976a9952a7c22a8d1866e89f5033ced4771
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2013-03-12 22:12:44 +0000

    F: add usability improvements to IP support

--- net.sf.seesea.ip/src/wizard/SelectHostPage.java
+++ net.sf.seesea.ip/src/wizard/SelectHostPage.java
@@ -51,6 +51,8 @@ public class SelectHostPage extends WizardPage implements IValidatingPage {
 	private int port;
 	private Text text;
 	private Text text_1;
+	private Text text_2;
+	private int timeout;
 
 	/**
 	 * @param serialNMEAProvider 
@@ -74,14 +76,14 @@ public class SelectHostPage extends WizardPage implements IValidatingPage {
 		
 		Label lblHost = new Label(container, SWT.NONE);
 		FormData fd_lblHost = new FormData();
-		fd_lblHost.top = new FormAttachment(0, 10);
 		fd_lblHost.left = new FormAttachment(0, 10);
 		lblHost.setLayoutData(fd_lblHost);
 		lblHost.setText(Messages.getString("SelectHostPage.host")); //$NON-NLS-1$
 		
 		text = new Text(container, SWT.BORDER);
+		fd_lblHost.top = new FormAttachment(text, 3, SWT.TOP);
 		FormData fd_text = new FormData();
-		fd_text.left = new FormAttachment(100, -523);
+		fd_text.left = new FormAttachment(100, -476);
 		fd_text.top = new FormAttachment(0, 10);
 		fd_text.right = new FormAttachment(100, -10);
 		text.setLayoutData(fd_text);
@@ -96,22 +98,55 @@ public class SelectHostPage extends WizardPage implements IValidatingPage {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				host = ((Text)e.widget).getText();
-				settings.put("lastHost", host);
+				settings.put("lastHost", host); //$NON-NLS-1$
 			}
 		});
 		
 		Label lblPort = new Label(container, SWT.NONE);
 		FormData fd_lblPort = new FormData();
-		fd_lblPort.top = new FormAttachment(text, 14);
-		fd_lblPort.left = new FormAttachment(0, 10);
+		fd_lblPort.left = new FormAttachment(lblHost, 0, SWT.LEFT);
 		lblPort.setLayoutData(fd_lblPort);
 		lblPort.setText(Messages.getString("SelectHostPage.port")); //$NON-NLS-1$
 		
 		text_1 = new Text(container, SWT.BORDER);
+		fd_lblPort.top = new FormAttachment(text_1, 3, SWT.TOP);
 		FormData fd_text_1 = new FormData();
-		fd_text_1.bottom = new FormAttachment(lblPort, 0, SWT.BOTTOM);
+		fd_text_1.top = new FormAttachment(text, 6);
 		fd_text_1.left = new FormAttachment(text, 0, SWT.LEFT);
 		text_1.setLayoutData(fd_text_1);
+		
+		Label lblTimeout = new Label(container, SWT.NONE);
+		FormData fd_lblTimeout = new FormData();
+		fd_lblTimeout.top = new FormAttachment(lblPort, 15);
+		fd_lblTimeout.left = new FormAttachment(0, 10);
+		lblTimeout.setLayoutData(fd_lblTimeout);
+		lblTimeout.setText(Messages.getString("SelectHostPage.lblTimeout.text")); //$NON-NLS-1$
+		
+		text_2 = new Text(container, SWT.BORDER);
+		try {
+			int lasttimeout = settings.getInt("timeout"); //$NON-NLS-1$
+			if(timeout != 0) {
+				text_2.setText(new Integer(lasttimeout).toString());
+				timeout = lasttimeout;
+			} else {
+				text_2.setText("10000"); //$NON-NLS-1$
+			}
+		} catch (NumberFormatException e) {
+			text_2.setText("10000"); //$NON-NLS-1$
+		}
+		text_2.addModifyListener(new ModifyListener() {
+			
+			@Override
+			public void modifyText(ModifyEvent e) {
+				timeout = Integer.parseInt(((Text)e.widget).getText());
+				settings.put("timeout", timeout); //$NON-NLS-1$
+			}
+		});
+		
+		FormData fd_text_2 = new FormData();
+		fd_text_2.top = new FormAttachment(text_1, 6);
+		fd_text_2.left = new FormAttachment(text, 0, SWT.LEFT);
+		text_2.setLayoutData(fd_text_2);
 		try {
 			int lastPort = settings.getInt("lastPort"); //$NON-NLS-1$
 			if(lastPort != 0) {
@@ -127,7 +162,7 @@ public class SelectHostPage extends WizardPage implements IValidatingPage {
 			public void modifyText(ModifyEvent e) {
 				try {
 					port = Integer.parseInt(((Text)e.widget).getText());
-					settings.put("lastPort", port);
+					settings.put("lastPort", port); //$NON-NLS-1$
 				} catch (NumberFormatException e2) {
 					// nothing to do;
 				}
@@ -142,28 +177,15 @@ public class SelectHostPage extends WizardPage implements IValidatingPage {
 	 */
 	@Override
 	public IStatus validatePage() {
-//		if(getPorts().isEmpty()) {
-//			setErrorMessage("No COM Ports available. Probably other software is using the COM port.");
-//			return new Status(IStatus.ERROR, "org.qbang.rxtx", "No COM Ports available"); //$NON-NLS-1$
-//		}
-//		IStructuredSelection structuredSelection = (IStructuredSelection) tableViewer.getSelection();
-//		if(!structuredSelection.isEmpty()) {
-//			CommPortIdentifier portIdentifier = (CommPortIdentifier) structuredSelection.getFirstElement();
-//			try {
-//				CommPort commPort = portIdentifier.open(getClass().getName(), 2000);
-//				if(commPort instanceof ParallelPort) {
-//					commPort.close();
-//					return new Status(IStatus.ERROR, "org.qbang.rxtx", Messages.getString("SelectComPortPage.noParallelPorts")); //$NON-NLS-1$ //$NON-NLS-2$
-//				} else {
-//					commPort.close();
-//					commPortIdentifier = portIdentifier;
-					return new Status(IStatus.OK, "org.qbang.rxtx", "FIXME"); //$NON-NLS-1$ //$NON-NLS-2$
-//				}
-//			} catch (PortInUseException e) {
-//				return new Status(IStatus.ERROR, "org.qbang.rxtx", Messages.getString("SelectComPortPage.failedOpen"), e); //$NON-NLS-1$ //$NON-NLS-2$
-//			}
-//		}
-//		return new Status(IStatus.ERROR, "org.qbang.rxtx", Messages.getString("SelectComPortPage.nothingSelected")); //$NON-NLS-1$ //$NON-NLS-2$;
+		if(host == null || host.isEmpty()) {
+			setErrorMessage(Messages.getString("SelectHostPage.invalidHost")); //$NON-NLS-1$
+			return new Status(IStatus.ERROR, "net.sf.seesea.ip", Messages.getString("SelectHostPage.invalidHost1")); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		if(port < 1 || port > 65535) {
+			setErrorMessage(Messages.getString("SelectHostPage.invalidPort")); //$NON-NLS-1$
+			return new Status(IStatus.ERROR, "net.sf.seesea.ip", Messages.getString("SelectHostPage.invalidPort1")); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		return new Status(IStatus.OK, "net.sf.seesea.ip", "FIXME"); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	public String getHost() {
@@ -173,4 +195,10 @@ public class SelectHostPage extends WizardPage implements IValidatingPage {
 	public int getPort() {
 		return port;
 	}
+
+	public int getTimeout() {
+		return timeout;
+	}
+	
+	
 }
