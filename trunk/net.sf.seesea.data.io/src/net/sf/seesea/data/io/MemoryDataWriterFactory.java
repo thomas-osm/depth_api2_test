64ff7f6c97e7f87ba9694e12a458eacbe19adccc
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2013-03-28 22:01:46 +0000

    F: move memory data writer

---
+++ trunk/net.sf.seesea.data.io/src/net/sf/seesea/data/io/MemoryDataWriterFactory.java
@@ -0,0 +1,36 @@
+package net.sf.seesea.data.io;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class MemoryDataWriterFactory implements IWriterFactory {
+
+	private List<IDataWriter> createDatawriters;
+
+	public MemoryDataWriterFactory() {
+		createDatawriters = new ArrayList<IDataWriter>();
+	}
+	
+	@Override
+	public IDataReader createReader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDataWriter createWriter(Map<String, String> parameters) {
+		MemoryDataWriter memoryDataWriter = new MemoryDataWriter();
+		createDatawriters.add(memoryDataWriter);
+		return memoryDataWriter;
+	}
+
+	protected List<IDataWriter> getCreatedDatawriters() {
+		return createDatawriters;
+	}
+	
+	
+	
+
+}
