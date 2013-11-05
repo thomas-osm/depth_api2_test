b7591f05a8237b58b618370fcae6831685e832f6
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2013-11-05 20:04:13 +0000

    F: Mavenify project

---
+++ trunk/org.osm.depth.upload/src/main/resources/org/osm/depth/upload/messages/VesselConfiguration.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ Copyright (c) 2010-2013, Jens K�bler All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. Neither the name of the <organization> nor the names
+ * of its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+package org.osm.depth.upload.messages;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="vesselconfig")
+public class VesselConfiguration {
+
+	public long id;
+	public String name;
+	public String description;
+	public double depthm;
+	public double depthd;
+	public String esinfront;
+	public String esrightof;
+	public double esdisy;
+	public double esdisx;
+	public double height;
+	public String yachtmodel;
+	public double slidingsp;
+	public String username;
+
+
+	
+    @XmlElementWrapper
+	@XmlAnyElement
+	public List<Offset> sbasOffsets;
+	
+
+    @XmlElementWrapper
+	@XmlAnyElement
+	public List<Offset> depthSensorOffsets;
+
+}
