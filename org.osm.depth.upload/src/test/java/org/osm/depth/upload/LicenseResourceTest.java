f6f6a1af311f6abbf3c6dc6e61744e0cfc49fb47
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2013-11-06 21:46:10 +0000

    R: move test folder

---
+++ org.osm.depth.upload/src/test/java/org/osm/depth/upload/LicenseResourceTest.java
@@ -0,0 +1,82 @@
+package org.osm.deoth.upload;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import junit.framework.TestCase;
+
+import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
+import org.glassfish.jersey.client.filter.HttpBasicAuthFilter;
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.osm.depth.upload.messages.License;
+
+public class LicenseResourceTest extends TestCase {
+	
+	public void testTrackUploadRoundTrip() throws FileNotFoundException {
+		Client client = ClientBuilder.newClient();
+		client.register(new HttpBasicAuthFilter("x", "x"));
+		client.register(MOXyJsonProvider.class);
+		client.register(MultiPartFeature.class);
+
+		LoggingFilter loggingFilter = new LoggingFilter();
+		client.register(loggingFilter);
+		
+		WebTarget basePath = client.target("http://localhost:8080").path("org.osm.depth.upload").path("api2");
+		WebTarget licensePath = basePath.path("license");
+		
+		License license3 = new License();
+		license3.name = "FreeBSD V1";
+		license3.publicLicense = true;
+		license3.shortName = "FreeBSD";
+		license3.text = "Text text";
+//		license3.User = "x";
+		
+		Response response = licensePath.request(MediaType.APPLICATION_JSON).post(Entity.entity(license3, MediaType.APPLICATION_JSON));
+		assertEquals(200, response.getStatus());
+		
+//		License license = response.readEntity(License.class);
+		
+//        BufferedReader bufferedReader = new BufferedReader(new FileInputStream("res/license.txt"));
+//        byte buf[] = new byte[4096];
+//        StringBuffer text = new StringBuffer();
+//        while(bufferedReader.read(buf) != -1) {
+//        	text.append(Charbuf.) 
+//        }
+        
+        response = licensePath.request(MediaType.APPLICATION_JSON).get();
+        assertEquals(200, response.getStatus());
+        List<License> licenses = response.readEntity(new GenericType<List<License>>(){});
+        long licenseId = 0;
+        for (License license2 : licenses) {
+			assertEquals(license2.name,license3.name);
+			assertEquals(license2.shortName,license3.shortName);
+			assertEquals(license2.publicLicense,license3.publicLicense);
+			assertEquals(license2.text,license3.text);
+			assertEquals(license2.User,"x");
+			assertNotSame(0, license2.id);
+			licenseId = license2.id;
+		}
+        
+        WebTarget deletePath = licensePath.path("/" + new Long(licenseId).toString()); //$NON-NLS-1$
+        response = deletePath.request().delete();
+        assertEquals(200, response.getStatus());
+
+        response = licensePath.request(MediaType.APPLICATION_JSON).get();
+        assertEquals(200, response.getStatus());
+        licenses = response.readEntity(new GenericType<List<License>>(){});
+        assertTrue(licenses.isEmpty());
+        
+		return;
+		
+	}
+}
