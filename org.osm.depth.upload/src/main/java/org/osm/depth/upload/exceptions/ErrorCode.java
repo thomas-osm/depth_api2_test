42b4b6bdd129233fcc621d722bbd4bd5fa0dbe0d
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2014-02-02 14:13:06 +0000

    F#https://github.com/OpenSeaMap/depth/issues/18, https://github.com/OpenSeaMap/depth/issues/17: Support email change, Support password reset

---
+++ org.osm.depth.upload/src/main/java/org/osm/depth/upload/exceptions/ErrorCode.java
@@ -0,0 +1,27 @@
+package org.osm.depth.upload.exceptions;
+
+@SuppressWarnings("nls")
+public enum ErrorCode {
+	
+	/** No old password supplied */
+	NO_OLD_PASSWORD("100:No old password supplied"),
+	/** No new password supplied */
+	NO_NEW_PASSWORD("101:No new password supplied"), 
+	
+	OLD_PASSWORD_MISMATCH("102:Old Password mismatch"), 
+	NO_SUCH_USER("103:No such user");
+	
+	private final String code;
+
+	ErrorCode(String code) {
+		this.code = code;
+	}
+	
+	@Override
+	public String toString() {
+		return code;
+	}
+	
+	
+
+}
