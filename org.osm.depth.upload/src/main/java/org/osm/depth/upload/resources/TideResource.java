302c49f4dfcbb59773c561d834532dbd063358a4
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2014-02-03 08:33:26 +0000

    F: add tide prediction

--- org.osm.depth.upload/src/main/java/org/osm/depth/upload/resources/TideResource.java
+++ org.osm.depth.upload/src/main/java/org/osm/depth/upload/resources/TideResource.java
@@ -1,26 +1,27 @@
 package org.osm.depth.upload.resources;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
+
+import net.sf.seesea.tidemodel.dtu10.JDummy;
 
 import org.osm.depth.upload.exceptions.DatabaseException;
-import org.osm.depth.upload.messages.GaugeMeasurement;
 import org.osm.depth.upload.messages.LengthUnit;
 import org.osm.depth.upload.messages.Tide;
 import org.osm.depth.upload.messages.TideLevel;
@@ -29,7 +30,8 @@ import org.osm.depth.upload.messages.TideLevel;
 public class TideResource {
 	
 	
-	public List<Tide> getTideHeights(double lat, double lon,TideLevel level, Date startDate, Date endDate) {
+	@GET
+	public List<Tide> getTideHeights(@QueryParam("lat") double lat, @QueryParam("lon") double lon,@QueryParam("tidelevel") TideLevel level, @QueryParam("startdate") long startDate, @QueryParam("enddate") long endDate, @QueryParam("unit") @DefaultValue("METERS") LengthUnit unit) {
 		JDummy tidePredictor = new JDummy();
 		List<Tide> list = new ArrayList<Tide>();
 		Context initContext;
@@ -40,30 +42,85 @@ public class TideResource {
 			try {
 				Statement statement = conn.createStatement();
 				try {
-					long timespan = endDate.getTime() - startDate.getTime();
+					long timespan = new Date(endDate).getTime() - new Date(startDate).getTime();
 					for(long i = 0; i < timespan ; i = i + 60000L ) {
-						Date date = new Date(startDate.getTime() +  i);
+						Date date = new Date(startDate +  i);
+
+						
 						double tideHeight = tidePredictor.getTideHeight(lat, lon, date);
 						Tide tide = new Tide();
 						tide.timestamp = date;
 						if(TideLevel.MEANSEALEVEL.equals(level)) {
+							switch (unit) {
+							case FEET:
+								tide.value = (float) (3.2808399 * tideHeight / 100);
+								break;
+							default:
+								tide.value = (float) tideHeight / 100;
+								break;
+							}
 						} else {
-							tide.value = (float) tideHeight;
+							double x1 = lon - Math.floor(lon);
+							int x2 = (int) (x1 / 0.125);
+							double a1 = x2 * 0.125 + Math.floor(lon);
+							double a2 = (x2+1) * 0.125 + Math.floor(lon);
+							
+							double y1 = lat - Math.floor(lat) ;
+							int y2 = (int) (y1 / 0.125);
+							double b1 = y2 * 0.125 + Math.floor(lat);
+							double b2 = (y2+1) * 0.125 + Math.floor(lat);
+							
+							List<Double> points = new ArrayList<Double>(4);
+							PreparedStatement preparedStatement = conn.prepareStatement("SELECT latoffset FROM msl2lat WHERE geom = ST_GeomFromText(?, 4326)"); //$NON-NLS-1$
+							preparedStatement.setString(1, MessageFormat.format("Point({0} {1})", a1,
+									b1));
+							ResultSet resultSet = preparedStatement.executeQuery();
+							while(resultSet.next()) {
+								points.add(resultSet.getDouble(1)); 
+							}
+							preparedStatement.setString(1, MessageFormat.format("Point({0} {1})", a1,
+									b2));
+							resultSet = preparedStatement.executeQuery();
+							while(resultSet.next()) {
+								points.add(resultSet.getDouble(1)); 
+							}
+							preparedStatement.setString(1, MessageFormat.format("Point({0} {1})", a2,
+									b1));
+							resultSet = preparedStatement.executeQuery();
+							while(resultSet.next()) {
+								points.add(resultSet.getDouble(1)); 
+							}
+							preparedStatement.setString(1, MessageFormat.format("Point({0} {1})", a2,
+									b2));
+							resultSet = preparedStatement.executeQuery();
+							while(resultSet.next()) {
+								points.add(resultSet.getDouble(1)); 
+							}
+							double average = 0.0;
+							for (Double point : points) {
+								average+= point;
+							}
+							switch (unit) {
+							case FEET:
+								tide.value = (float) (3.2808399 * ( tideHeight / 100 + (average / points.size()) * -1));
+								break;
+							default:
+								tide.value =  (float) (tideHeight / 100 + (average / points.size()) * -1);
+								break;
+							}
 						}
-						
-						double restlat = lat - Math.floor(lat / 0.125) * 0.125;
-						double lat1 = lat - restlat;
-						double lat2 = lat + 0.125;
-					}
-					
-					ResultSet executeQuery = statement.executeQuery("SELECT * FROM gaugemeasurement g ORDER BY time"); //$NON-NLS-1$
-					
-					while(executeQuery.next()) {
-						Tide tide = new Tide();
-						tide.value = executeQuery.getFloat("value");
-						tide.timestamp = new Date(executeQuery.getTimestamp("time").getTime());
 						list.add(tide);
 					}
+					
+					
+//					ResultSet executeQuery = statement.executeQuery("SELECT * FROM gaugemeasurement g ORDER BY time"); //$NON-NLS-1$
+//					
+//					while(executeQuery.next()) {
+//						Tide tide = new Tide();
+//						tide.value = executeQuery.getFloat("value");
+//						tide.timestamp = new Date(executeQuery.getTimestamp("time").getTime());
+//						list.add(tide);
+//					}
 					return list;
 				} finally {
 					statement.close();
