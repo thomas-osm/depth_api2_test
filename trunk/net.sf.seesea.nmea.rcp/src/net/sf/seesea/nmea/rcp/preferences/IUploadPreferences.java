3c8eda2e52d97aaa8e73558b65d212bfe9849f27
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2014-02-11 20:29:43 +0000

    F: remove wizard from nmea rcp

--- trunk/net.sf.seesea.nmea.rcp/src/net/sf/seesea/nmea/rcp/preferences/IUploadPreferences.java
+++
@@ -1,8 +0,0 @@
-package net.sf.seesea.nmea.rcp.preferences;
-
-public interface IUploadPreferences {
-	
-	String UPLOAD_SERVER = "UploadServer"; //$NON-NLS-1$
-
-	String REGISTER_URL = "registerURL"; //$NON-NLS-1$
-}
