b51c6387d15bd14185a0c51caa9be29ce085bbf6
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2014-02-11 17:57:51 +0000

    F#https://github.com/OpenSeaMap/depth/issues/13: Admins may download files for review

---
+++ org.osm.depth.upload/src/main/java/org/osm/depth/upload/resources/FileStreamingOutput.java
@@ -0,0 +1,30 @@
+package org.osm.depth.upload.resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
+
+public class FileStreamingOutput implements StreamingOutput {
+	
+	private final File file;
+
+	public FileStreamingOutput(File file) {
+		this.file = file;
+	}
+
+	@Override
+	public void write(OutputStream out) throws IOException,
+			WebApplicationException {
+		FileInputStream in = new FileInputStream(file);
+		byte[] buffer = new byte[16384];
+		int len;
+		while ((len = in.read(buffer)) != -1) {
+		    out.write(buffer, 0, len);
+		}
+	}
+
+}
