eb589281660617863caaa078fd2e2a40e2c40314
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2014-04-28 07:34:44 +0000

    F: provide acceleration interfaces

---
+++ net.sf.seesea.services.navigation/src/net/sf/seesea/services/navigation/listener/IAccelerationListener.java
@@ -0,0 +1,7 @@
+package net.sf.seesea.services.navigation.listener;
+
+import net.sf.seesea.model.core.physx.Acceleration;
+
+public interface IAccelerationListener extends IDataListener<Acceleration> {
+
+}
---
+++ net.sf.seesea.services.navigation/src/net/sf/seesea/services/navigation/provider/TideLevel.java
@@ -0,0 +1,9 @@
+package net.sf.seesea.services.navigation.provider;
+
+import javax.xml.bind.annotation.XmlEnum;
+
+@XmlEnum
+public enum TideLevel {
+
+	MEANSEALEVEL,LOWESTATRONOMICALTIDE;
+}
