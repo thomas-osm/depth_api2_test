3b71aa65f5bbe3a8260aa8090a349a7961d1cf4d
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2014-07-07 21:11:57 +0000

    R: rename base class for event dispatching

--- trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/NMEAMessageValidator.java
+++ trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/NMEAMessageValidator.java
@@ -1,6 +1,6 @@
 /**
  * 
-Copyright (c) 2010-2012, Jens Kübler
+Copyright (c) 2010-2012, Jens Kï¿½bler
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -39,7 +39,7 @@ public class NMEAMessageValidator {
 	 * @param rawContent
 	 * @return
 	 */
-	protected boolean removeValidChecksum(String nmeaMessageContent) {
+	protected boolean xremoveValidChecksum(String nmeaMessageContent) {
 		if(!nmeaMessageContent.startsWith("$")) { //$NON-NLS-1$
 			return false;
 		}
