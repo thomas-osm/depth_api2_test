ea5d4efc6cc3f6dece6cacf1336b6e2e0a3ff005
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2014-11-02 15:29:49 +0000

    T: provide test data

--- trunk/net.sf.seesea.provider.navigation.fsh.test/src/net/sf/seesea/provider/navigation/fsh/test/FSHStreamProcessorTest.java
+++ trunk/net.sf.seesea.provider.navigation.fsh.test/src/net/sf/seesea/provider/navigation/fsh/test/FSHStreamProcessorTest.java
@@ -3,10 +3,14 @@ package net.sf.seesea.provider.navigation.fsh.test;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import junit.framework.TestCase;
 import net.sf.seesea.provider.navigation.fsh.FSHStreamProcessor;
+import net.sf.seesea.provider.navigation.fsh.FlobHeader;
+import net.sf.seesea.provider.navigation.fsh.data.FSHBlock;
+import net.sf.seesea.provider.navigation.fsh.data.FSHHeader;
 import net.sf.seesea.services.navigation.NMEAProcessingException;
 
 public class FSHStreamProcessorTest extends TestCase {
@@ -24,5 +28,29 @@ public class FSHStreamProcessorTest extends TestCase {
 		FSHStreamProcessor fshStreamProcessor = new FSHStreamProcessor();
 		assertTrue(fshStreamProcessor.isValidStreamProcessor(buf));
 	}
+	
+	public void testX() throws IOException {
+		String file = "res/219.dat"; //$NON-NLS-1$
+		System.out.println(new File(file).getAbsoluteFile());
+		BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
+		FSHStreamProcessor fshStreamProcessor = new FSHStreamProcessor();
+		FSHHeader fshHeader = fshStreamProcessor.readHeader(input);
+		int hallo = 0;
+		
+		for(int i = 0 ; i < 8 ; i++) {
+			FlobHeader flobHeader = fshStreamProcessor.readFlobHeader(input);
+			FSHBlock fshBlock = fshStreamProcessor.readBlock(input);
+			while(fshBlock != null) {
+				byte[] message = new byte[fshBlock.getLength()];
+				input.read(message);
+				System.out.println("Type" + fshBlock.getType() + " : Length" + fshBlock.getLength());
+				fshBlock = fshStreamProcessor.readBlock(input);
+				hallo++;
+			}
+		}
+		
+	}
+	
+	
 	
 }
