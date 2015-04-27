6074c748f5b1d82256425bb13e5ec92d4d9e1dc7
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2015-04-27 18:59:09 +0000

    F: Add triangulation plugins

---
+++ trunk/net.sf.seesea.triangulation.cdelaunay/src/net/sf/seesea/triangulation/cdelaunay/Quadtree.java
@@ -0,0 +1,237 @@
+/**
+Copyright (c) 2013-2015, Jens Kübler
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package net.sf.seesea.triangulation.cdelaunay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.seesea.geometry.IBoxExtends;
+import net.sf.seesea.geometry.IRectangle;
+import net.sf.seesea.geometry.impl.Rectangle;
+
+
+/**
+ * A 2D Quadtree implementation that is a spatial index structure.
+ * 
+ * @param <T>
+ */
+public class Quadtree<T extends IBoxExtends> {
+
+	 private int MAX_OBJECTS = 1000;
+	  private int MAX_LEVELS = 20;
+	 
+	  private int level;
+	  private List<T> objects;
+	  private IRectangle bounds;
+	  private Quadtree[] nodes;
+	 
+	 /*
+	  * Constructor
+	  */
+	  public Quadtree(int pLevel, IRectangle pBounds) {
+	   level = pLevel;
+	   objects = new ArrayList();
+	   bounds = pBounds;
+	   nodes = new Quadtree[4];
+	  }
+	  
+	  /*
+	   * Clears the quadtree
+	   */
+	   public void clear() {
+	     objects.clear();
+	   
+	     for (int i = 0; i < nodes.length; i++) {
+	       if (nodes[i] != null) {
+	         nodes[i].clear();
+	         nodes[i] = null;
+	       }
+	     }
+	   }
+	   
+	   /*
+	    * Splits the node into 4 subnodes
+	    */
+	    private void split() {
+	    	   System.out.println("split:" + bounds);
+
+	      double subWidth = (bounds.getWidth() / 2);
+	      double subHeight = (bounds.getHeight() / 2);
+	      double x = bounds.getX();
+	      double y = bounds.getY();
+	    
+	      nodes[0] = new Quadtree(level+1, new Rectangle(x + subWidth, y, subWidth, subHeight));
+	      nodes[1] = new Quadtree(level+1, new Rectangle(x, y, subWidth, subHeight));
+	      nodes[2] = new Quadtree(level+1, new Rectangle(x, y + subHeight, subWidth, subHeight));
+	      nodes[3] = new Quadtree(level+1, new Rectangle(x + subWidth, y + subHeight, subWidth, subHeight));
+	    }
+	    
+	    /*
+	     * Determine which node the object belongs to. -1 means
+	     * object cannot completely fit within a child node and is part
+	     * of the parent node
+	     */
+	     private int getIndex(IBoxExtends boundingBox) {
+	    	 IRectangle pRect = boundingBox.getBoundingBox();
+	       int index = -1;
+	       double verticalMidpoint = bounds.getX() + (bounds.getWidth() / 2);
+	       double horizontalMidpoint = bounds.getY() + (bounds.getHeight() / 2);
+	     
+	       // Object can completely fit within the top quadrants
+	       boolean topQuadrant = (pRect.getY() < horizontalMidpoint && pRect.getY() + pRect.getHeight() < horizontalMidpoint);
+	       // Object can completely fit within the bottom quadrants
+	       boolean bottomQuadrant = (pRect.getY() > horizontalMidpoint);
+	     
+	       // Object can completely fit within the left quadrants
+	       if (pRect.getX() < verticalMidpoint && pRect.getX() + pRect.getWidth() < verticalMidpoint) {
+	          if (topQuadrant) {
+	            index = 1;
+	          }
+	          else if (bottomQuadrant) {
+	            index = 2;
+	          }
+	        }
+	        // Object can completely fit within the right quadrants
+	        else if (pRect.getX() > verticalMidpoint) {
+	         if (topQuadrant) {
+	           index = 0;
+	         }
+	         else if (bottomQuadrant) {
+	           index = 3;
+	         }
+	       }
+	     
+	       return index;
+	     }
+
+	      public void remove(T pRect) {
+//	    	   System.out.println("remove:" + pRect);
+		        if (nodes[0] != null) {
+			          int index = getIndex(pRect);
+			      
+			          if (index != -1) {
+			            nodes[index].remove(pRect);
+			      
+			            return;
+			          }
+			        }
+		        boolean remove = objects.remove(pRect);
+		        if(!remove)
+		        System.out.println(pRect);
+	      }
+	     
+	     /*
+	      * Insert the object into the quadtree. If the node
+	      * exceeds the capacity, it will split and add all
+	      * objects to their corresponding nodes.
+	      */
+	      public void insert(T pRect) {
+//	    	   System.out.println("insert:" + pRect);
+	        if (nodes[0] != null) {
+	          int index = getIndex(pRect);
+	      
+	          if (index != -1) {
+	            nodes[index].insert(pRect);
+	      
+	            return;
+	          }
+	        }
+	      
+	        objects.add(pRect);
+	      
+	        if (objects.size() > MAX_OBJECTS && level < MAX_LEVELS) {
+	           if (nodes[0] == null) { 
+	              split(); 
+	           }
+	      
+	          int i = 0;
+	          while (i < objects.size()) {
+	            int index = getIndex(objects.get(i));
+	            if (index != -1) {
+	              nodes[index].insert(objects.remove(i));
+	            }
+	            else {
+	              i++;
+	            }
+	          }
+	        }
+	      }
+	      
+	      /*
+	       * Return all objects that could collide with the given object
+	       */
+	       public void retrieve(List<T> returnObjects, IBoxExtends pRect) {
+//	    	   System.out.println("ret:" + pRect);
+	         int index = getIndex(pRect);
+	         if (index != -1 && nodes[0] != null) {
+//	        	 System.out.println("->" + index + ":" + bounds);
+	           nodes[index].retrieve(returnObjects, pRect);
+	         }
+	       
+	         returnObjects.addAll(objects);
+	       }
+	       
+	       public int size() {
+	    	   if(nodes[0] == null) {
+	    		   return objects.size();
+	    	   }
+	    	   return objects.size() + nodes[0].size() + nodes[1].size() + nodes[2].size() + nodes[3].size();   
+	       }
+	      
+	       public void getObjectLocation(T object) {
+	    	   
+	       }
+
+	       public int getObjectLocation2(T object) {
+	    	   if(objects.contains(object)) {
+//				   System.out.println("->-1:" + bounds); 
+	    		   return -1;
+	    	   } else {
+	    		   if(nodes[0] != null) {
+	    			   for (int i = 0; i < nodes.length; i++) {
+	    				   int objectLocation2 = nodes[i].getObjectLocation2(object);
+	    				   if(objectLocation2 != -2) {
+//	    					   System.out.println("->" + i + ":" + bounds); 
+	    					   return i;
+	    				   }
+	    			   }
+	    		   }
+	    	   }
+	    	   return -2;
+	       }
+
+	       public void stats(List<Integer> sizes) {
+    		   if(nodes[0] != null) {
+    			   for (int i = 0; i < nodes.length; i++) {
+    				   sizes.add(nodes[i].objects.size());
+    				   nodes[i].stats(sizes);
+    			   }
+    		   }
+	       }
+	       
+}
---
+++ trunk/net.sf.seesea.triangulation/src/net/sf/seesea/triangulation/ITriangulationPersistence.java
@@ -0,0 +1,118 @@
+/**
+Copyright (c) 2013-2015, Jens Kübler
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package net.sf.seesea.triangulation;
+
+import java.util.Iterator;
+import java.util.List;
+
+import net.sf.seesea.data.io.PersistenceException;
+import net.sf.seesea.geometry.IPoint;
+import net.sf.seesea.geometry.IPolygon;
+import net.sf.seesea.geometry.ITriangle;
+
+
+public interface ITriangulationPersistence {
+
+	/**
+	 * This methods calculates the partitions that the recorded track crosses. 
+	 * 
+	 * @param trackId the track id to determine the partitions
+	 * @param trackpointTable the sql table to use for reading measured points
+	 * @return A {@link PolygonIterator} that iterates over the existing partitions
+	 * @throws PersistenceException
+	 */
+	Iterator<List<IPolygon>> getHitPartitionizedPolygons(Long trackId, String trackpointTable)
+			throws PersistenceException;
+
+	/**
+	 * 
+	 * 
+	 * @param hitPartitionizedPolygons
+	 * @param trackpointTable
+	 * @return
+	 * @throws PersistenceException
+	 */
+	List<ITriangulationDescription> updateContainedTriangulations(Iterator<List<IPolygon>> hitPartitionizedPolygons, String trackpointTable) throws PersistenceException;
+
+	/**
+	 * persists the given triangulation
+	 * 
+	 * @param triangulationResult the triangulation to persist
+	 * @param triangulationDescription the description with boundaries and holes
+	 * @throws PersistenceException
+	 */
+	void persistTriangulation(List<ITriangle> triangulationResult, ITriangulationDescription triangulationDescription) throws PersistenceException;
+	
+	/**
+	 * This method is used to split contour line that were merged over delaunay triangulation partitions. 
+	 * The result of the operation is that no contour line crosses the given boundary. 
+	 * 
+	 * @param trackId
+	 * @param boundary the boundary of the contour line
+	 * @throws PersistenceException if a persistence error occurs
+	 */
+	void splitMergedContourLines(Long trackId, IPolygon boundary) throws PersistenceException;
+
+	/**
+	 * This method creates a new contour line with the given points or updates
+	 * 
+	 * @param points the points of the contour line
+	 * @param depth the depth of the contour line
+	 * @param trackId the track id causing the update
+	 * @param boundary the partition boundary 
+	 * @throws PersistenceException
+	 */
+	void addOrUpdateContourLine(List<IPoint> points, Integer depth, Long trackId, IPolygon boundary) throws PersistenceException;
+
+	/**
+	 * Removes contours lines in the given polygon with holes
+	 * 
+	 * @param polygon the boundary polygon
+	 * @param holes the holes in the boundary polygon
+	 * @throws PersistenceException
+	 */
+	void removeContourLines(IPolygon polygon, List<IPolygon> holes) throws PersistenceException;
+
+
+	/**
+	 * 
+	 * @return the first polygon is the bounding polygon where the rest are contained polygons
+	 */
+	Iterator<List<IPolygon>> getRiverBoundaryPolygonsIterator();
+	
+	List<IPolygon> get100mBoundaryPolygons();
+	
+	List<IPolygon> getCostalBorderBoundaryHoles(IPolygon boundary100m);
+	
+	List<ITriangle> getExistingTriangulation(IPolygon area);
+
+	List<ITriangulationDescription> getPartitionizedTriangulations(Long trackId, String trackpointTable) throws PersistenceException;
+
+
+
+}
