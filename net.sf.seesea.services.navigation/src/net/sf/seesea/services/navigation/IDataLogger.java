3d48a632e95f3b202b2d9cad85f151e8da8921c4
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2015-08-09 20:29:31 +0000

    F: Provide service interface for data logging

---
+++ net.sf.seesea.services.navigation/src/net/sf/seesea/services/navigation/IDataLogger.java
@@ -0,0 +1,20 @@
+package net.sf.seesea.services.navigation;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+
+public interface IDataLogger {
+
+	void suspendPersistentLogging() throws NMEAProcessingException;
+	
+	void resumePersistentLogging();
+	
+	List<File> getRecordedFiles();
+	
+	IStatus archiveRecordedFiles(final List<File> recordedFiles,
+			final MultiStatus status, IProgressMonitor subProgressMonitor);
+}
