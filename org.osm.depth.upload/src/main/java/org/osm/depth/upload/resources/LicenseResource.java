7fe09da5a83843320fe27c954e12aaad7152efff
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-03-17 21:40:04 +0000

    R: use autoclosables and raise to java 7

--- org.osm.depth.upload/src/main/java/org/osm/depth/upload/resources/LicenseResource.java
+++ org.osm.depth.upload/src/main/java/org/osm/depth/upload/resources/LicenseResource.java
@@ -15,7 +15,6 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -45,30 +44,21 @@ public class LicenseResource {
 		try {
 			initContext = new InitialContext();
 			DataSource ds = (DataSource)initContext.lookup("java:/comp/env/jdbc/postgres"); //$NON-NLS-1$
-			Connection conn = ds.getConnection();
-			try {
-				ResultSet executeQuery;
+			try (Connection conn = ds.getConnection()) {
 				if(context.isUserInRole("ADMIN")) { //$NON-NLS-1$
-					Statement statement = conn.createStatement();
-					try {
-						executeQuery = statement.executeQuery("SELECT * FROM license l LEFT OUTER JOIN user_profiles u ON u.user_name = l.user_name ORDER BY l.shortname,l.name"); //$NON-NLS-1$
-						return getLicenses(context, executeQuery);
-					} finally {
-						statement.close();
+					try (Statement statement = conn.createStatement()) {
+						try (ResultSet executeQuery = statement.executeQuery("SELECT * FROM license l LEFT OUTER JOIN user_profiles u ON u.user_name = l.user_name ORDER BY l.shortname,l.name")) {
+							return getLicenses(context, executeQuery);
+						}
 					}
 				} else {
-					PreparedStatement pStatement = conn.prepareStatement("SELECT * FROM license l LEFT OUTER JOIN vesselconfiguration u ON u.user_name = l.user_name WHERE l.user_name= ? OR l.public = true ORDER BY l.shortname,l.name"); //$NON-NLS-1$
-					pStatement.setString(1, username);
-					try {
-						executeQuery = pStatement.executeQuery();
-						return getLicenses(context, executeQuery);
-					} finally {
-						pStatement.close();
+					try (PreparedStatement pStatement = conn.prepareStatement("SELECT * FROM license l LEFT OUTER JOIN vesselconfiguration u ON u.user_name = l.user_name WHERE l.user_name= ? OR l.public = true ORDER BY l.shortname,l.name")) {
+						pStatement.setString(1, username);
+						try (ResultSet executeQuery = pStatement.executeQuery()) {
+							return getLicenses(context, executeQuery);
+						}
 					}
 				}
-				
-			} finally {
-				conn.close();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -98,14 +88,8 @@ public class LicenseResource {
 					license.user = "Other";
 				}
 			}
-			
-//					UriBuilder ub = uriInfo.getBaseUriBuilder();
-//					track.delete = ub.path("/track/" + track.id).build().toString(); //$NON-NLS-1$
 			list.add(license);
 		}
-//				GenericEntity<List<Track>> entity = new GenericEntity<List<Track>>(list) {/* */};
-//				Link self = Link.fromMethod(TrackResource.class,"delete").build(); //$NON-NLS-1$
-//				Response response = Response.ok().entity(entity).links(self).build();
 		return list;
 	}
 	
@@ -128,18 +112,14 @@ public class LicenseResource {
 			initContext = new InitialContext();
 			DataSource dsDepth = (DataSource)initContext.lookup("java:/comp/env/jdbc/depth"); //$NON-NLS-1$
 			DataSource ds = (DataSource)initContext.lookup("java:/comp/env/jdbc/postgres"); //$NON-NLS-1$
-			Connection depthConn = dsDepth.getConnection();
-			try {
-				Connection conn = ds.getConnection();
-				try {
-					ResultSet executeQuery;
-					PreparedStatement statement = depthConn.prepareStatement("SELECT DISTINCT datasetid FROM trackpoints_raw_8 WHERE trackpoints_raw_8.the_geom && ST_MakeEnvelope(?, ?, ?, ?, 4326)"); //$NON-NLS-1$
-					try {
-						statement.setDouble(1, Double.parseDouble(lon1));
-						statement.setDouble(2, Double.parseDouble(lat1));
-						statement.setDouble(3, Double.parseDouble(lon2));
-						statement.setDouble(4, Double.parseDouble(lat2));
-						executeQuery = statement.executeQuery();
+			try (Connection depthConn = dsDepth.getConnection();
+				 Connection conn = ds.getConnection()) {
+				try (PreparedStatement statement = depthConn.prepareStatement("SELECT DISTINCT datasetid FROM trackpoints_raw_8 WHERE trackpoints_raw_8.the_geom && ST_MakeEnvelope(?, ?, ?, ?, 4326)")){
+					statement.setDouble(1, Double.parseDouble(lon1));
+					statement.setDouble(2, Double.parseDouble(lat1));
+					statement.setDouble(3, Double.parseDouble(lon2));
+					statement.setDouble(4, Double.parseDouble(lat2));
+					try (ResultSet executeQuery = statement.executeQuery()) {
 						StringBuffer buffer = new StringBuffer();
 						while(executeQuery.next()) {
 							buffer.append(executeQuery.getString(1));
@@ -150,33 +130,24 @@ public class LicenseResource {
 						}
 						// we have trackpoints there
 						if(buffer.length() > 0) {
-							PreparedStatement licenseStatement = conn.prepareStatement("SELECT shortname FROM license INNER JOIN (SELECT DISTINCT license FROM user_tracks WHERE track_id IN (" + buffer.toString() + ") ) l2 ON license.id = l2.license"); //$NON-NLS-1$
-							try {
-								ResultSet licensesResult = licenseStatement.executeQuery();
-								buffer = new StringBuffer();
-								while(licensesResult.next()) {
-									buffer.append(licensesResult.getString(1));
-									buffer.append(',');
+							try (PreparedStatement licenseStatement = conn.prepareStatement("SELECT shortname FROM license INNER JOIN (SELECT DISTINCT license FROM user_tracks WHERE track_id IN (" + buffer.toString() + ") ) l2 ON license.id = l2.license")) {
+								try (ResultSet licensesResult = licenseStatement.executeQuery()) {
+									buffer = new StringBuffer();
+									while(licensesResult.next()) {
+										buffer.append(licensesResult.getString(1));
+										buffer.append(',');
+									}
+									if(buffer.length() > 0) {
+										buffer.deleteCharAt(buffer.length() - 1);
+									}
+									return buffer.toString();
 								}
-								if(buffer.length() > 0) {
-									buffer.deleteCharAt(buffer.length() - 1);
-								}
-								return buffer.toString();
-							} finally {
-								licenseStatement.close();
 							}
 						}
 						// return empty string for no licenses
 						return ""; //$NON-NLS-1$
-					} finally {
-						statement.close();
 					}
-					
-				} finally {
-					conn.close();
 				}
-			} finally {
-				depthConn.close();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -197,41 +168,28 @@ public class LicenseResource {
 		String username = context.getUserPrincipal().getName();
 		Context initContext;
 		try {
-			
 			initContext = new InitialContext();
 			DataSource ds = (DataSource)initContext.lookup("java:/comp/env/jdbc/postgres"); //$NON-NLS-1$
-			Connection conn = ds.getConnection();
-			try {
-				Statement createIDStatement = conn.createStatement();
-				try {
-					PreparedStatement statement = conn.prepareStatement("INSERT INTO license (id, user_name, name, shortname, text, public) VALUES (?,?,?,?,?,?)"); //$NON-NLS-1$
-//			Statement userIDQueryStatement = conn.createStatement();
-					try {
-						ResultSet executeQuery = createIDStatement.executeQuery("SELECT nextval('license_id_seq')"); //$NON-NLS-1$
-						if(executeQuery.next()) {
-							Long id = executeQuery.getLong(1);
-							statement.setLong(1, id);
-							statement.setString(2, username);
-							statement.setString(3, license.name);
-							statement.setString(4, license.shortName);
-							statement.setString(5, license.text);
-							statement.setBoolean(6, license.publicLicense);
-							statement.execute();
-							return id.toString();
-						} else {
-							// failed to create id
-						}
-						throw new DatabaseException("Database unavailable"); //$NON-NLS-1$
-					} finally {
-						statement.close();
+			try (Connection conn = ds.getConnection()){
+				try (Statement createIDStatement = conn.createStatement();
+					 PreparedStatement statement = conn.prepareStatement("INSERT INTO license (id, user_name, name, shortname, text, public) VALUES (?,?,?,?,?,?)");
+					 ResultSet executeQuery = createIDStatement.executeQuery("SELECT nextval('license_id_seq')")) {
+					if(executeQuery.next()) {
+						Long id = executeQuery.getLong(1);
+						statement.setLong(1, id);
+						statement.setString(2, username);
+						statement.setString(3, license.name);
+						statement.setString(4, license.shortName);
+						statement.setString(5, license.text);
+						statement.setBoolean(6, license.publicLicense);
+						statement.execute();
+						return id.toString();
+					} else {
+						// failed to create id
 					}
-				} finally {
-					createIDStatement.close();
+					throw new DatabaseException("Database unavailable"); //$NON-NLS-1$
 				}
-			} finally {
-				conn.close();
 			}
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DatabaseException("Internal SQL Error"); //$NON-NLS-1$
@@ -251,19 +209,15 @@ public class LicenseResource {
 			long licenseId = Long.parseLong(id);
 			initContext = new InitialContext();
 			DataSource ds = (DataSource)initContext.lookup("java:/comp/env/jdbc/postgres"); //$NON-NLS-1$
-			Connection conn = ds.getConnection();
-			try {
+			try (Connection conn = ds.getConnection()){
 				conn.setAutoCommit(false);
-				PreparedStatement licenseUsedinTracks = conn.prepareStatement("SELECT COUNT(track_id) FROM user_tracks WHERE license = ? "); //$NON-NLS-1$
-				try {
+				try (PreparedStatement licenseUsedinTracks = conn.prepareStatement("SELECT COUNT(track_id) FROM user_tracks WHERE license = ? ");
+					 Statement deletestatement = conn.createStatement()) {
 					licenseUsedinTracks.setLong(1, licenseId);
-					Statement deletestatement = conn.createStatement();
-					try {
-						ResultSet usedInTracksResultSet = licenseUsedinTracks.executeQuery();
+					try (ResultSet usedInTracksResultSet = licenseUsedinTracks.executeQuery()){
 						if(usedInTracksResultSet.next() && usedInTracksResultSet.getLong(1) > 0) {
 							throw new ResourceInUseException("License is still being used for recorded tracks");
 						}
-						
 						if(context.isUserInRole("ADMIN")) { //$NON-NLS-1$
 							deletestatement.execute(MessageFormat
 									.format("DELETE FROM license WHERE id = {0}", //$NON-NLS-1$
@@ -275,17 +229,9 @@ public class LicenseResource {
 						}
 						conn.commit();
 						return Response.ok().build();
-					} finally {
-						deletestatement.close();
 					}
-				} finally {
-					licenseUsedinTracks.close();
 				}
-			} finally {
-				conn.close();
 			}
-
-	
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DatabaseException("Internal SQL Error"); //$NON-NLS-1$
