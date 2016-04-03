138d8b251f1e7b7c85dfdf68e1d876103505fc5d
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-04-03 12:27:14 +0000

    R: remove warnings

--- net.sf.seesea.provider.navigation.fsh.test/src/net/sf/seesea/provider/navigation/fsh/test/FSHStreamProcessorTest.java
+++ net.sf.seesea.provider.navigation.fsh.test/src/net/sf/seesea/provider/navigation/fsh/test/FSHStreamProcessorTest.java
@@ -1,8 +1,6 @@
 package net.sf.seesea.provider.navigation.fsh.test;
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
