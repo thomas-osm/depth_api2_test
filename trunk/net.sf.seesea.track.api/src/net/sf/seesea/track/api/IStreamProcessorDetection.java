86fd7e89eb07a70cab1c6c691fbadb2ce54ac4c9
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-04-22 06:02:15 +0000

    F: Add content detection as module and service

--- trunk/net.sf.seesea.track.api/src/net/sf/seesea/track/api/IStreamProcessorDetection.java
+++ trunk/net.sf.seesea.track.api/src/net/sf/seesea/track/api/IStreamProcessorDetection.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import net.sf.seesea.track.api.exception.NMEAProcessingException;
+import net.sf.seesea.track.api.exception.RawDataEventException;
 
 public interface IStreamProcessorDetection {
 
 	IStreamProcessor detectStreamProcessorEnblock(InputStream inputStream, boolean log)
-			throws IOException, NMEAProcessingException;
+			throws IOException, RawDataEventException;
 
 	IStreamProcessor detectBinaryStreamProcessorEnblock(InputStream inputStream, boolean log)
-			throws IOException, NMEAProcessingException;
+			throws IOException, RawDataEventException;
 
 }
