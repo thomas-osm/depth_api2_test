83af0ba0a786a9e84644a06ccae4118a07093834
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-04-30 12:43:35 +0000

    R: fix utf8

--- net.sf.seesea.geometry/src/net/sf/seesea/geometry/impl/Triangle.java
+++ net.sf.seesea.geometry/src/net/sf/seesea/geometry/impl/Triangle.java
@@ -1,6 +1,5 @@
 /**
- * 
-Copyright (c) 2014, Jens Kübler
+Copyright (c) 2013-2015, Jens Kübler
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -24,7 +23,7 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 
 package net.sf.seesea.geometry.impl;
