9cc2f96473f50c41270591a1e46013719d126357
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-04-30 13:01:45 +0000

    R: fix utf8

--- trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/NMEASentence.java
+++ trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/NMEASentence.java
@@ -1,6 +1,6 @@
 /**
  * 
-Copyright (c) 2010-2012, Jens Kübler
+Copyright (c) 2010-2012, Jens KÃ¼bler
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
--- trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/v2000/datadictionary/Data.java
+++ trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/v2000/datadictionary/Data.java
@@ -1,6 +1,6 @@
 /**
  * 
-Copyright (c) 2010-2012, Jens K�bler
+Copyright (c) 2010-2012, Jens Kübler
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
