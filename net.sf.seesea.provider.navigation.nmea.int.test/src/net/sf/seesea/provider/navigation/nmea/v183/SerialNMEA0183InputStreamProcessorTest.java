c3664ee5a9a80da1aa0460ba51a86daaeb19f3ff
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-05-02 18:13:07 +0000

    T: provide test feature

---
+++ net.sf.seesea.provider.navigation.nmea.int.test/src/net/sf/seesea/provider/navigation/nmea/v183/SerialNMEA0183InputStreamProcessorTest.java
@@ -0,0 +1,35 @@
+package net.sf.seesea.provider.navigation.nmea.v183;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.junit.Test;
+
+import net.sf.seesea.provider.navigation.nmea.NMEA0183Activator;
+import net.sf.seesea.track.api.exception.RawDataEventException;
+
+public class SerialNMEA0183InputStreamProcessorTest {
+
+	@Test
+	public void testStreamProcessor() throws IOException, RawDataEventException {
+		String file = "res/9220.dat"; //$NON-NLS-1$
+		URL fileEntry = NMEA0183Activator.getContext().getBundle().getEntry(file);
+		InputStream fileStream = fileEntry.openStream();
+		BufferedInputStream input = new BufferedInputStream(fileStream);
+
+		int[] buf = new int[100];
+		for(int i = 0; i < 100 ; i ++) {
+			buf[i] = input.read();
+		}
+		
+		SerialNMEA0183InputStreamProcessor serialNMEA0183InputStreamProcessor = new SerialNMEA0183InputStreamProcessor();
+		boolean validStreamProcessor = serialNMEA0183InputStreamProcessor.isValidStreamProcessor(buf);
+		
+		assertTrue(validStreamProcessor);
+	}
+	
+}
