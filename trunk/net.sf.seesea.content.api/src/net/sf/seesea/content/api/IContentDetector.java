fe0750c1362da42076f82ef7bc34f8d4e04546e9
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-05-09 07:14:53 +0000

    R: add javadoc

--- trunk/net.sf.seesea.content.api/src/net/sf/seesea/content/api/IContentDetector.java
+++ trunk/net.sf.seesea.content.api/src/net/sf/seesea/content/api/IContentDetector.java
@@ -1,7 +1,15 @@
 package net.sf.seesea.content.api;
 
+/**
+ * Implementors of this interface to uploaded data content detection
+ *
+ */
 public interface IContentDetector {
 
+	/**
+	 * This method determines the content types of the uploaded track files. It does no postprocessing or statistical analysis.
+	 * @throws ContentDetectionException if it fails to start content detection.
+	 */
 	void setContentTypes() throws ContentDetectionException;
 
 }
