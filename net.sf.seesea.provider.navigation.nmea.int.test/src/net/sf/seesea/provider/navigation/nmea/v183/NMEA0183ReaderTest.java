4c7d6c0e95f264411b090126d18b10a768d40339
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-05-10 05:42:02 +0000

    R: remove unused code

--- net.sf.seesea.provider.navigation.nmea.int.test/src/net/sf/seesea/provider/navigation/nmea/v183/NMEA0183ReaderTest.java
+++ net.sf.seesea.provider.navigation.nmea.int.test/src/net/sf/seesea/provider/navigation/nmea/v183/NMEA0183ReaderTest.java
@@ -3,7 +3,6 @@ package net.sf.seesea.provider.navigation.nmea.v183;
 import static org.junit.Assert.assertEquals;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
@@ -13,14 +12,9 @@ import org.junit.Test;
 
 import net.sf.seesea.model.core.physx.Measurement;
 import net.sf.seesea.provider.navigation.nmea.NMEA0183Activator;
-import net.sf.seesea.provider.navigation.nmea.NMEA0183EventProcessor;
 
 public class NMEA0183ReaderTest {
 
-	public NMEA0183ReaderTest() {
-		// TODO Auto-generated constructor stub
-	}
-	
 	@Test
 	public void testReaderContent() throws IOException {
 		URL fileEntry = NMEA0183Activator.getContext().getBundle().findEntries("res", "9220.dat", false).nextElement();
