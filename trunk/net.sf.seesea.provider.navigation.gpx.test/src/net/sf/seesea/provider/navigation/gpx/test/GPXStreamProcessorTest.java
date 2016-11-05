7e6c5fd6fcb5b91203f042b672af07e3ca3885ad
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-11-05 08:25:22 +0000

    B: Close streams correctly

--- trunk/net.sf.seesea.provider.navigation.gpx.test/src/net/sf/seesea/provider/navigation/gpx/test/GPXStreamProcessorTest.java
+++ trunk/net.sf.seesea.provider.navigation.gpx.test/src/net/sf/seesea/provider/navigation/gpx/test/GPXStreamProcessorTest.java
@@ -20,15 +20,16 @@ public class GPXStreamProcessorTest {
 	public void testStreamProcessor() throws RawDataEventException, IOException {
 		URL fileEntry = GPXTestActivator.getContext().getBundle().findEntries("res", "8629.dat", false).nextElement();
 		InputStream fileStream = FileLocator.resolve(fileEntry).openStream();
-		BufferedInputStream input = new BufferedInputStream(fileStream);
-
-		int[] buf = new int[2048];
-		for(int i = 0; i < 2048 ; i ++) {
-			buf[i] = input.read();
+		try(BufferedInputStream input = new BufferedInputStream(fileStream)) {
+			int[] buf = new int[2048];
+			for(int i = 0; i < 2048 ; i ++) {
+				buf[i] = input.read();
+			}
+			
+			GPXStreamProcessor gpxStreamProcessor = new GPXStreamProcessor();
+			assertTrue(gpxStreamProcessor.isValidStreamProcessor(buf));
 		}
 
-		GPXStreamProcessor gpxStreamProcessor = new GPXStreamProcessor();
-		assertTrue(gpxStreamProcessor.isValidStreamProcessor(buf));
 	}
 
 }
