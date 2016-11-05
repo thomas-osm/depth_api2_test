806b9bc64d642f65519a64c50fb9e8fc890185af
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-11-05 08:30:09 +0000

    B: Close streams correctly

--- net.sf.seesea.provider.navigation.son.int.test/src/net/sf/seesea/navigation/son/SONStreamProcessorTest.java
+++ net.sf.seesea.provider.navigation.son.int.test/src/net/sf/seesea/navigation/son/SONStreamProcessorTest.java
@@ -1,7 +1,5 @@
 package net.sf.seesea.navigation.son;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,18 +15,19 @@ public class SONStreamProcessorTest {
 	@Test
 	public void testStreamProcessor() throws IOException, RawDataEventException {
 		URL fileEntry = SONActivator.getContext().getBundle().findEntries("res", "9171.dat", false).nextElement();
-		InputStream fileStream = FileLocator.resolve(fileEntry).openStream();
-		BufferedInputStream input = new BufferedInputStream(fileStream);
-
-		int[] buf = new int[2048];
-		for(int i = 0; i < 2048 ; i ++) {
-			buf[i] = input.read();
-		}
-		
-		SONStreamProcessor serialNMEA0183InputStreamProcessor = new SONStreamProcessor();
-		boolean validStreamProcessor = serialNMEA0183InputStreamProcessor.isValidStreamProcessor(buf);
-		
+		try(InputStream fileStream = FileLocator.resolve(fileEntry).openStream();
+		BufferedInputStream input = new BufferedInputStream(fileStream)) {
+			int[] buf = new int[2048];
+			for(int i = 0; i < 2048 ; i ++) {
+				buf[i] = input.read();
+			}
+			
+			SONStreamProcessor serialNMEA0183InputStreamProcessor = new SONStreamProcessor();
+			boolean validStreamProcessor = serialNMEA0183InputStreamProcessor.isValidStreamProcessor(buf);
 //		assertTrue(validStreamProcessor);
+		}
+
+		
 	}
 	
 }
