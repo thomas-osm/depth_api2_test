462d10b14b443515f281c5cbc3311b890e5e0da4
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2016-11-05 08:24:04 +0000

    B: Close streams correctly

--- net.sf.seesea.provider.navigation.fsh.test/src/net/sf/seesea/provider/navigation/fsh/test/FSHStreamProcessorTest.java
+++ net.sf.seesea.provider.navigation.fsh.test/src/net/sf/seesea/provider/navigation/fsh/test/FSHStreamProcessorTest.java
@@ -10,7 +10,6 @@ import net.sf.seesea.provider.navigation.fsh.FSHStreamProcessor;
 import net.sf.seesea.provider.navigation.fsh.FlobHeader;
 import net.sf.seesea.provider.navigation.fsh.data.FSHBlock;
 import net.sf.seesea.provider.navigation.fsh.data.FSHHeader;
-import net.sf.seesea.track.api.exception.NMEAProcessingException;
 import net.sf.seesea.track.api.exception.RawDataEventException;
 
 public class FSHStreamProcessorTest extends TestCase {
@@ -19,35 +18,37 @@ public class FSHStreamProcessorTest extends TestCase {
 		String file = "res/archive2.fsh"; //$NON-NLS-1$
 		URL fileEntry = FshTestActivator.getContext().getBundle().getEntry(file);
 		InputStream fileStream = fileEntry.openStream();
-		BufferedInputStream input = new BufferedInputStream(fileStream);
-
-		int[] buf = new int[100];
-		for(int i = 0; i < 100 ; i ++) {
-			buf[i] = input.read();
+		try(BufferedInputStream input = new BufferedInputStream(fileStream)) {
+			int[] buf = new int[100];
+			for(int i = 0; i < 100 ; i ++) {
+				buf[i] = input.read();
+			}
+			
+			FSHStreamProcessor fshStreamProcessor = new FSHStreamProcessor();
+			assertTrue(fshStreamProcessor.isValidStreamProcessor(buf));
 		}
-		
-		FSHStreamProcessor fshStreamProcessor = new FSHStreamProcessor();
-		assertTrue(fshStreamProcessor.isValidStreamProcessor(buf));
+
 	}
 	
 	public void testBasicFileRead() throws IOException {
 		String file = "res/archive2.fsh"; //$NON-NLS-1$
 		URL fileEntry = FshTestActivator.getContext().getBundle().getEntry(file);
 		InputStream fileStream = fileEntry.openStream();
-		BufferedInputStream input = new BufferedInputStream(fileStream);
-		FSHStreamProcessor fshStreamProcessor = new FSHStreamProcessor();
-		FSHHeader fshHeader = fshStreamProcessor.readHeader(input);
-		
-		for(int i = 0 ; i < 8 ; i++) {
-			FlobHeader flobHeader = fshStreamProcessor.readFlobHeader(input);
-			FSHBlock fshBlock = fshStreamProcessor.readBlock(input);
-			while(fshBlock != null) {
-				byte[] message = new byte[fshBlock.getLength()];
-				input.read(message);
-				System.out.println("Type" + fshBlock.getType() + " : Length" + fshBlock.getLength());
-				fshBlock = fshStreamProcessor.readBlock(input);
-				if(fshBlock != null) {
-					assertTrue("Block types should exist", fshBlock.getType() == 13 || fshBlock.getType() == 14 || fshBlock.getType() == 65535);
+		try (BufferedInputStream input = new BufferedInputStream(fileStream)) {
+			FSHStreamProcessor fshStreamProcessor = new FSHStreamProcessor();
+			FSHHeader fshHeader = fshStreamProcessor.readHeader(input);
+			
+			for(int i = 0 ; i < 8 ; i++) {
+				FlobHeader flobHeader = fshStreamProcessor.readFlobHeader(input);
+				FSHBlock fshBlock = fshStreamProcessor.readBlock(input);
+				while(fshBlock != null) {
+					byte[] message = new byte[fshBlock.getLength()];
+					input.read(message);
+					System.out.println("Type" + fshBlock.getType() + " : Length" + fshBlock.getLength());
+					fshBlock = fshStreamProcessor.readBlock(input);
+					if(fshBlock != null) {
+						assertTrue("Block types should exist", fshBlock.getType() == 13 || fshBlock.getType() == 14 || fshBlock.getType() == 65535);
+					}
 				}
 			}
 		}
