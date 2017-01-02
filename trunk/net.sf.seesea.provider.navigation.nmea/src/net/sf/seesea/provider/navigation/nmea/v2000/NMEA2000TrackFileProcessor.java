b18872a3222e199f3257056748b42a0a478517e7
cleanerx <cleanerx@bdda867f-eb90-41bb-8c09-ca81aeede8af>
2017-01-02 22:44:24 +0000

    B: use correct track file processor

--- trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/v2000/NMEA2000TrackFileProcessor.java
+++ trunk/net.sf.seesea.provider.navigation.nmea/src/net/sf/seesea/provider/navigation/nmea/v2000/NMEA2000TrackFileProcessor.java
@@ -18,7 +18,7 @@ import net.sf.seesea.track.api.data.SensorDescriptionUpdateRate;
 import net.sf.seesea.track.api.exception.InputStreamNotFoundException;
 import net.sf.seesea.track.api.exception.ProcessingException;
 
-@Component(factory = "trackfile.x-nmea2000")
+@Component(factory = "trackfile.x-actisense")
 public class NMEA2000TrackFileProcessor implements ITrackFileProcessor,
 		INMEA2000Listener {
 
